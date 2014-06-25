374bccf28bf641a98d40705605730d5d0550575c
vagrant <vagrant@precise64.(none)>
2014-06-25 18:15:06 +0000

    fixed bower dependencies

--- organizer/settings.py
+++ organizer/settings.py
@@ -92,7 +92,8 @@ TEMPLATE_DIRS = (
 from django.conf import global_settings
 STATICFILES_FINDERS = global_settings.STATICFILES_FINDERS + ('djangobower.finders.BowerFinder', )
 
-BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, "static", "components")
+BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, "static")
+
 
 BOWER_INSTALLED_APPS = (
     'jquery#1.9',
