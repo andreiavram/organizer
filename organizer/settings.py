d5104bf014d713c6b52e085761292aa58d2c08a0
Andrei Avram <andrei.avram@gmail.com>
2015-11-04 10:33:21 +0200

    updates global settings import

--- organizer/settings.py
+++ organizer/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-from django.conf.global_settings import STATICFILES_DIRS, LOGIN_REDIRECT_URL
+from django.conf import global_settings
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
